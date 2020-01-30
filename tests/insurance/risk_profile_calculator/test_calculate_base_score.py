fe95d8021b5d6d61b845c362c985fa430df3773d
Felipe Rodrigues <felipe@crafters.cc>
2020-01-29 23:51:33 -0300

    Removing bloated fields from profile data for base score test

--- tests/insurance/risk_profile_calculator/test_calculate_base_score.py
+++ tests/insurance/risk_profile_calculator/test_calculate_base_score.py
@@ -2,11 +2,9 @@ def test__calculate_base_score_with_no_special_age(calculator):
     profile_data = {
         "age": 41,
         "dependents": 2,
-        "house": {"ownership_status": "owned"},
         "income": 0,
         "marital_status": "married",
         "risk_questions": [1, 1, 1],
-        "vehicle": {"year": 2018},
     }
 
     assert calculator._RiskCalculator__calculate_base_score(profile_data) == 3
@@ -16,11 +14,9 @@ def test__calculate_base_score_with_age_under_30(calculator):
     profile_data = {
         "age": 21,
         "dependents": 2,
-        "house": {"ownership_status": "owned"},
         "income": 200001,
         "marital_status": "married",
         "risk_questions": [0, 0, 0],
-        "vehicle": {"year": 2018},
     }
 
     assert calculator._RiskCalculator__calculate_base_score(profile_data) == -2
@@ -30,11 +26,9 @@ def test__calculate_base_score_with_age_between_30_and_40(calculator):
     profile_data = {
         "age": 35,
         "dependents": 2,
-        "house": {"ownership_status": "owned"},
         "income": 100000,
         "marital_status": "married",
         "risk_questions": [0, 0, 0],
-        "vehicle": {"year": 2018},
     }
 
     assert calculator._RiskCalculator__calculate_base_score(profile_data) == -1
@@ -44,11 +38,9 @@ def test__calculate_base_score_with_age_eq_40(calculator):
     profile_data = {
         "age": 40,
         "dependents": 2,
-        "house": {"ownership_status": "owned"},
         "income": 100000,
         "marital_status": "married",
         "risk_questions": [0, 0, 0],
-        "vehicle": {"year": 2018},
     }
 
     assert calculator._RiskCalculator__calculate_base_score(profile_data) == -1
@@ -58,11 +50,9 @@ def test__calculate_base_score_with_age_eq_30(calculator):
     profile_data = {
         "age": 40,
         "dependents": 2,
-        "house": {"ownership_status": "owned"},
         "income": 100000,
         "marital_status": "married",
         "risk_questions": [0, 0, 0],
-        "vehicle": {"year": 2018},
     }
 
     assert calculator._RiskCalculator__calculate_base_score(profile_data) == -1
@@ -72,11 +62,9 @@ def test__calculate_base_score_with_negative_risk_questions(calculator):
     profile_data = {
         "age": 45,
         "dependents": 2,
-        "house": {"ownership_status": "owned"},
         "income": 100000,
         "marital_status": "married",
         "risk_questions": [-1, -1, -1],
-        "vehicle": {"year": 2018},
     }
 
     assert calculator._RiskCalculator__calculate_base_score(profile_data) == -3
